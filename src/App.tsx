762aede67c8e93c2c4931af130f459804bcb859a
Lukasz Kuczynski <lukasz.kuczynski@bpol.net>
2022-09-01 15:37:07 +0200

    delete: unnecessary background component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme';
 import SearchBar from 'components/SearchBar';
-import Background from 'components/Background';
 import SearchResults from 'components/SearchResults';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Background />
       <SearchBar />
       <SearchResults />
     </ThemeProvider>
